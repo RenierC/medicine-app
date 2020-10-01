daa6a800f8eb7699f47a4c37d796dc3e098c6468
RenierC <reniercuervo@gmail.com>
2020-10-01 12:00:03 -0400

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Table from "./components/Table";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Basket />
+          </Route>
+          <Route path="/">
+            <Table />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
